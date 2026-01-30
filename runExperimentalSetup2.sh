b221688fe861c5c783908af1816880017d1bd747
Thibault Marette <marette@login1.head.dardel.pdc.kth.se>
2026-01-30 17:18:21 +0100

    updating setups

--- runExperimentalSetup2.sh
+++ runExperimentalSetup2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 declare -a historicals=("Resilient" "Carv" "FFT")
-declare -a datasets=("Uber" "Twitter" "OnlineRetail" "Electricity")
+#declare -a datasets=("Uber" "Twitter" "OnlineRetail" "Electricity")
 #Uber and Twitter need to be installed manually.
-#declare -a datasets=("OnlineRetail" "Electricity")
+declare -a datasets=("OnlineRetail" "Electricity")
 declare -a ks=(30)
 
 for k in "${ks[@]}"
@@ -15,4 +15,4 @@ do
                 done
         done
 done
-python3 src/plotsForSetup2.py
+#python3 src/plotsForSetup2.py
